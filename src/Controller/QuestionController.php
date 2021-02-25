d951c2717300b6afe4b10eb0979d08b50fd20d6e
Kélian <kelian.raoult_severac@edu.devinci.fr>
2021-02-25 16:31:52 +0100

    REMOVE - addQuestionController to make CRUD inside questionController

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Questions;
+use App\Form\AskQuestionsType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -54,5 +56,39 @@ class QuestionController extends AbstractController
                 "newAnswer" => $form->createView(),
             ]
         );
+    }
+
+    /**
+     * @Route("/ask_question", name="ask_question")
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+
+        $user = $this->getUser();
+
+        if ($user !== null) {
+
+            $question = new Questions;
+
+            $form = $this->createForm(AskQuestionsType::class, $question);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $question->setQuestionIsResolved(0);
+                $question->setQuestionAnswers(0);
+                $question->setQuestionLikes(0);
+                $question->setQuestionAuthor($user->getUsername());
+                $question->setQuestionDate(new \DateTime());
+                $em->persist($question);
+                $em->flush();
+
+                return $this->redirectToRoute("look_questions");
+            }
+            return $this->render('ask_question/ask_question.html.twig', [
+                "newQuestion" => $form->createView(),
+            ]);
+        } else {
+            return $this->redirectToRoute("app_login");
+        }
     }
 }
